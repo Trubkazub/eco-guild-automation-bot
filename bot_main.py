254305588be5f12599f03b28565d1bb453635dba
sirot <sirot60@gmail.com>
2022-11-24 22:41:11 +0300

    Created gitignore, requirements

---
+++ bot_main.py
@@ -0,0 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+async def main():
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
+    dp = Dispatcher(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
