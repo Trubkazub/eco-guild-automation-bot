b6eac529ebf98f74a3da5890acd65c18e4af1be5
Eugene <jenia.barykin@gmail.com>
2022-11-27 23:50:40 +0300

    Реализовал создание трех таблиц для сбора всей информации от пользователей

--- db/create_table.py
+++ db/create_table.py
@@ -1,36 +1,62 @@
+from dotenv import dotenv_values
 from connect_db import open_connection, close_connection
 
+config = dotenv_values(".env")
+
+USER = config['USER']
+HOST = config['HOST']
+PASSWORD = config['PASSWORD']
+PORT = config['PORT']
+DATABASE = config['DATABASE']
+
 def create_table(cursor):
-    sql_create_table = f'CREATE TABLE IF NOT EXISTS user_info (' \
-                       f'user_id serial PRIMARY KEY,' \
-                       f'username VARCHAR (50) UNIQUE NOT NULL,' \
-                       f'first_name VARCHAR (32) NOT NULL,' \
-                       f'patronymic VARCHAR (32),' \
-                       f'surname VARCHAR (32) NOT NULL,' \
-                       f'status VARCHAR (20) NOT NULL,' \
-                       f'university VARCHAR(250),' \
-                       f'department VARCHAR(250),' \
-                       f'year_of_studying VARCHAR(32),' \
-                       f'higher_education VARCHAR(50),' \
-                       f'having_degree BOOL,' \
-                       f'science_degree VARCHAR(50),' \
-                       f'phone_number VARCHAR(20),' \
-                       f'email VARCHAR(50),' \
-                       f'vk VARCHAR(50),' \
-                       f'drivers_license BOOL,' \
-                       f'carsharing BOOL,' \
-                       f'wanna_be_carshar BOOL' \
-                       f');'
+    users_simple_info = f'CREATE TABLE IF NOT EXISTS users_simple_info (' \
+                        f'user_id VARCHAR(32) UNIQUE PRIMARY KEY,' \
+                        f'username VARCHAR (50) UNIQUE NOT NULL,' \
+                        f'first_name VARCHAR (32),' \
+                        f'patronymic VARCHAR (32),' \
+                        f'surname VARCHAR (32),' \
+                        f'status VARCHAR (20) NOT NULL,' \
+                        f'phone_number VARCHAR (20),' \
+                        f'email VARCHAR (50),' \
+                        f'vk VARCHAR (50),' \
+                        f'drivers_license BOOL DEFAULT False,' \
+                        f'carsharing BOOL DEFAULT False,' \
+                        f'wanna_be_carshar BOOL DEFAULT False' \
+                        f');'
 
-    cursor.execute(sql_create_table)
-    print('sozdaltb')
+    schoolkid_info = f'CREATE TABLE IF NOT EXISTS schoolkid_info (' \
+                     f'user_id VARCHAR(32) UNIQUE PRIMARY KEY,' \
+                     f'username VARCHAR (50) UNIQUE NOT NULL,' \
+                     f'status VARCHAR (20) NOT NULL,' \
+                     f'school VARCHAR (250),' \
+                     f'grade INT,' \
+                     f'CONSTRAINT fk_user_id' \
+                     f'     FOREIGN KEY (user_id)' \
+                     f'          REFERENCES users_simple_info(user_id)' \
+                     f'          ON DELETE CASCADE ON UPDATE CASCADE' \
+                     f');'
 
+    students_info = f'CREATE TABLE IF NOT EXISTS student_info (' \
+                    f'user_id VARCHAR(32) UNIQUE PRIMARY KEY,' \
+                    f'username VARCHAR (50) UNIQUE NOT NULL,' \
+                    f'status VARCHAR (20) NOT NULL,' \
+                    f'university VARCHAR (250),' \
+                    f'department VARCHAR (250),' \
+                    f'year_of_studying VARCHAR (32),' \
+                    f'higher_education VARCHAR (50),' \
+                    f'having_degree BOOL,' \
+                    f'science_degree VARCHAR (50),' \
+                    f'CONSTRAINT fk_user_id' \
+                    f'   FOREIGN KEY (user_id) ' \
+                    f'       REFERENCES users_simple_info (user_id)' \
+                    f'       ON DELETE CASCADE ON UPDATE CASCADE' \
+                    f');'
 
-USER = 'postgres'
-HOST = '127.0.0.1'
-PASSWORD = 1234
-PORT = 5432
-DATABASE = 'eco_guild_mgu'
+    for sql_create_table in [users_simple_info, schoolkid_info, students_info]:
+        cursor.execute(sql_create_table)
+    print('sozdal_tbs')
+
 
 if __name__ == '__main__':
     connection = open_connection(USER, PASSWORD, HOST, PORT, DATABASE)
